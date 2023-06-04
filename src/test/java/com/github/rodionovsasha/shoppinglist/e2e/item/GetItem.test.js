acde2665725bbd6a70ee932f722a3294ca886f34
Juan Pablo Ramos Torres <61593415+Juan-dev123@users.noreply.github.com>
2023-06-04 18:11:33 -0500

    changed error messages

--- src/test/java/com/github/rodionovsasha/shoppinglist/e2e/item/GetItem.test.js
+++ src/test/java/com/github/rodionovsasha/shoppinglist/e2e/item/GetItem.test.js
@@ -53,7 +53,7 @@ describe('Verifying getting Process of an item', () => {
             expect(error.response.status).to.equal(StatusCodes.NOT_FOUND);
             expect(error.response.data).to.be.an('object');
             expect(error.response.data).to.have.property('message');
-            expect(error.response.data.message).to.equal("Item not found");
+            expect(error.response.data.message).to.equal("The entity with id '4' could not be found");
         }
     });
 
