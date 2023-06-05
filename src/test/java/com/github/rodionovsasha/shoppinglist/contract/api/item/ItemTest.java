ee2963c623ff921cc3ad700762e8cb8a0835daad
Alejandro.F.Forero <54719974+AlejandroFonseca25@users.noreply.github.com>
2023-06-05 17:53:02 -0500

    Fixed problem with get and update contract tests for item

--- src/test/java/com/github/rodionovsasha/shoppinglist/contract/api/item/ItemTest.java
+++ src/test/java/com/github/rodionovsasha/shoppinglist/contract/api/item/ItemTest.java
@@ -9,13 +9,10 @@ import au.com.dius.pact.provider.junitsupport.loader.PactBrokerAuth;
 import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
 
 import com.github.rodionovsasha.shoppinglist.controllers.ItemRestController;
-import com.github.rodionovsasha.shoppinglist.controllers.ItemsListRestController;
 import com.github.rodionovsasha.shoppinglist.dto.ItemDto;
-import com.github.rodionovsasha.shoppinglist.dto.ItemsListDto;
 import com.github.rodionovsasha.shoppinglist.entities.Item;
 import com.github.rodionovsasha.shoppinglist.entities.ItemsList;
 import com.github.rodionovsasha.shoppinglist.services.ItemService;
-import com.github.rodionovsasha.shoppinglist.services.ItemsListService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,8 +21,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
-import java.lang.reflect.Type;
-import java.util.Collections;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -62,7 +57,7 @@ public class ItemTest {
     @State("has item to get")
     public void getItem() {
         ItemsList listItem;
-        listItem = new ItemsList("List for Brekfast");
+        listItem = new ItemsList("List for Breakfast");
         listItem.setId(1);
         Item item;
         item = new Item("Orange 2k");
@@ -70,8 +65,15 @@ public class ItemTest {
         item.setComment("For juice");
         item.setBought(true);
         item.setItemsList(listItem);
+        ItemDto.GetResponse itemDto = new ItemDto.GetResponse();
+        itemDto.setId(1);
+        itemDto.setName("Orange 2k");
+        itemDto.setComment("For juice");
+        itemDto.setBought(true);
+        itemDto.setListId(1);
 
         Mockito.when(itemService.getItemById(Mockito.any(long.class))).thenReturn(item);
+        when(modelMapper.map(any(Item.class),Mockito.any())).thenReturn(itemDto);
     }
 
     @State("has item to update")
