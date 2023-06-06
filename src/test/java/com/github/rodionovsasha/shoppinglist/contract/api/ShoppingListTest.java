03555617f0743cb515a93194ac0436b5f303c78b
Alejandro.F.Forero <54719974+AlejandroFonseca25@users.noreply.github.com>
2023-06-06 14:09:43 -0500

    Merged two contract tests files

--- src/test/java/com/github/rodionovsasha/shoppinglist/contract/api/ShoppingListTest.java
+++ src/test/java/com/github/rodionovsasha/shoppinglist/contract/api/ShoppingListTest.java
@@ -1,4 +1,4 @@
-package com.github.rodionovsasha.shoppinglist.contract.api.item;
+package com.github.rodionovsasha.shoppinglist.contract.api;
 
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
@@ -8,10 +8,13 @@ import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
 import au.com.dius.pact.provider.junitsupport.loader.PactBrokerAuth;
 import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
 import com.github.rodionovsasha.shoppinglist.controllers.ItemRestController;
+import com.github.rodionovsasha.shoppinglist.controllers.ItemsListRestController;
 import com.github.rodionovsasha.shoppinglist.dto.ItemDto;
+import com.github.rodionovsasha.shoppinglist.dto.ItemsListDto;
 import com.github.rodionovsasha.shoppinglist.entities.Item;
 import com.github.rodionovsasha.shoppinglist.entities.ItemsList;
 import com.github.rodionovsasha.shoppinglist.services.ItemService;
+import com.github.rodionovsasha.shoppinglist.services.ItemsListService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,6 +23,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+
+import java.lang.reflect.Type;
+import java.util.Collections;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -28,7 +35,7 @@ import static org.mockito.Mockito.*;
         url = "${PACT_BROKER_BASE_URL}",
         authentication = @PactBrokerAuth(token = "${PACT_BROKER_TOKEN}"))
 @ExtendWith(MockitoExtension.class)
-public class ItemTest {
+public class ShoppingListTest {
 
     @Mock
     private ItemService itemService;
@@ -37,14 +44,19 @@ public class ItemTest {
     private ModelMapper modelMapper;
 
     @InjectMocks
-    private ItemRestController itemController;
+    private ItemRestController itemRestController;
 
+    @Mock
+    private ItemsListService itemsListService;
+
+    @InjectMocks
+    private ItemsListRestController itemsListRestController;
 
     @BeforeEach
     public void changeContext(PactVerificationContext context) {
-        System.setProperty("pact.verifier.publishResults", "true");
+        //System.setProperty("pact.verifier.publishResults", "true");
         MockMvcTestTarget testTarget = new MockMvcTestTarget();
-        testTarget.setControllers(itemController);
+        testTarget.setControllers(itemRestController,itemsListRestController);
         context.setTarget(testTarget);
     }
 
@@ -107,4 +119,52 @@ public class ItemTest {
         item.setItemsList(listItem);
         when(itemService.updateItem(any(long.class),any(long.class),any())).thenReturn(item);
     }
+
+    @State("has lists to get")
+    public void getAllLists() {
+        ItemsList itemsList = new ItemsList("Test list");
+        itemsList.setId(1L);
+        when(itemsListService.findAllLists()).thenReturn(Collections.singletonList(itemsList));
+        when(modelMapper.map(anyIterable(), any(Type.class))).thenReturn(Collections.singletonList(itemsList));
+    }
+
+    @State("there are no lists")
+    public void saveItemsList() {
+        when(modelMapper.map(any(ItemsListDto.class), any())).thenReturn(new ItemsList("Test list"));
+        when(itemsListService.addItemsList(any(ItemsList.class))).thenReturn(1L);
+    }
+
+    @State("has list to get")
+    public void getItemsList() {
+        Item item = new Item("Orange");
+        item.setId(2L);
+        item.setComment("For juice");
+        item.setBought(true);
+        ItemsList itemsList = new ItemsList();
+        itemsList.setId(1L);
+        itemsList.setName("Test list");
+        itemsList.setItems(Collections.singletonList(item));
+        ItemDto.GetInListResponse itemDto = new ItemDto.GetInListResponse();
+        itemDto.setId(2L);
+        itemDto.setName("Orange");
+        itemDto.setComment("For juice");
+        itemDto.setBought(true);
+        ItemsListDto.GetResponse getResponse = new ItemsListDto.GetResponse();
+        getResponse.setId(1L);
+        getResponse.setName("Test list");
+        getResponse.setItems(Collections.singletonList(itemDto));
+        when(itemsListService.getItemsListById(anyLong())).thenReturn(itemsList);
+        when(modelMapper.map(any(ItemsList.class), any())).thenReturn(getResponse);
+    }
+
+    @State("has list to delete")
+    public void deleteItemsList() {
+        doNothing().when(itemsListService).deleteItemsList(anyLong());
+    }
+
+    @State("has list to update")
+    public void updateItemsList() {
+        doNothing().when(itemsListService).updateItemsList(anyLong(), anyString());
+    }
+
 }
